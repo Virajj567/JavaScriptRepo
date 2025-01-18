6a6fb4d6f0df328163ba94fe08a8d0a5b8d68ded
VIRAJ JOSHI <102504892+Virajj567@users.noreply.github.com>
2025-01-18 10:36:18 +0000

    Till dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 0;
+let accountEmail = "viraj@gamil.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+
+
+// accountId = 2;
+accountEmail="vj@vj.com"
+accountPassword="21212"
+accountCity="Bengaluru"
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope.
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3);     we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Viraj");
+
+let name = "viraj"
+let age = 21
+let isLoggedIn = false
+
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique         using in react to find uniqueness
+// object
+
+
+
+console.log(typeof age); //number
+console.log(typeof null)  //object
+console.log(typeof undefined) //undefined
+
